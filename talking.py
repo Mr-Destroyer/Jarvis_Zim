59b81c353e0e9171a9f2fce5a56f3ea09d98a445
Mohammad Zim <88034756+Mr-Destroyer@users.noreply.github.com>
2021-07-28 21:37:03 +0600

    #Copyright at mohammad zim

---
+++ talking.py
@@ -0,0 +1,20 @@
+import pyttsx3
+
+
+def zim():
+    friend = pyttsx3.init()
+    speech = input ("say something: ")
+    friend.say(speech)
+    friend.runAndWait()
+zim()
+zim()
+zim()
+zim()
+zim()
+
+
+
+
+
+
+
