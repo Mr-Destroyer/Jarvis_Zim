a6d1dc8cbbfff0b605e0b87836b4d07492839a77
Mohammad Zim <88034756+Mr-Destroyer@users.noreply.github.com>
2021-07-28 22:03:55 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#Created by Mohammad Zim 
+#installing python
+apt-get install pv
+apt-get install python
+echo "Your Tool is installing by Mr_Destroyer" | pv -qL 10
+pip install pyttsx3
+echo "Tool is installed " | pv -qL 10
+read -p "press enter "
+clear
+
