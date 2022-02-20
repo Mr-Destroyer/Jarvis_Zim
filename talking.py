5422cfff245919fbcd17182cc7e1dd14662bf611
Mohammad Zim <88034756+Mr-Destroyer@users.noreply.github.com>
2022-02-20 22:25:33 +0600

    jarvis.py

--- talking.py
+++ talking.py
@@ -1,18 +1,11 @@
 import pyttsx3
 
-
-def zim():
-    friend = pyttsx3.init()
-    speech = input("say something: ")
-    friend.say(speech)
-    friend.runAndWait()
-zim()
-zim()
-zim()
-zim()
-zim()
-zim()
-zim()
+while True:
+    
+        friend = pyttsx3.init()
+        speech = input("say something: ")
+        friend.say(speech)
+        friend.runAndWait()
 
 
 
